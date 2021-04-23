c85cb93be6822217246af845a2c5ad831e2aafe0
Greg Murray <gd-murray@hotmail.com>
2021-04-22 23:51:14 -0400

    Slimmed out more

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ import 'firebase/auth'
 if (!firebase.apps.length) {
     firebase.initializeApp({
         apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        authDomain: `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
         projectId: process.env.FIREBASE_PROJECT_ID,
         appId: process.env.FIREBASE_APP_ID
     })
