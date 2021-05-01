d1800561af2270cfd314d21397fae0da64bfb1ff
Greg Murray <gd-murray@hotmail.com>
2021-04-30 22:59:13 -0400

    theres no way im this stupid lol

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -1,8 +1,6 @@
 import crypto from 'crypto'
 
 const algorithm = 'aes-256-ctr'
-const FIREBASE_KEY = 'HRfFS:@z2r9kM>2_<ijf~z=HH#-VkCZu'
-const GOOGLE_KEY = 's9nF~XU1ec:b2M><~pH8lVJxLwuBz.0!'
 const iv = crypto.randomBytes(16)
 
 export const decrypt = (hash, secret) => {
