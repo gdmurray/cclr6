de0515ab018eb35817f71a7d057b60a8b091078a
Greg Murray <gd-murray@hotmail.com>
2021-07-09 12:02:00 -0400

    gooby liked with controls

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -39,7 +39,7 @@ export default function Post(post: CMSPost): JSX.Element {
                                 playing
                                 width="100%"
                                 height="100%"
-                                controls={false}
+                                controls={true}
                             />
                         </div>
                     </div>
