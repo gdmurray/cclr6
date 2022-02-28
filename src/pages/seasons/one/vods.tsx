43fe5d50948830855ef7a5976c72654f9fa284f4
Greg Murray <gd-murray@hotmail.com>
2022-02-28 11:15:24 -0500

    fix season vods

--- src/pages/seasons/one/vods.tsx
+++ src/pages/seasons/one/vods.tsx
@@ -1,13 +1,5 @@
-import { GetStaticPathsResult, GetStaticPropsResult } from 'next'
-import { getSeasonPaths } from '@lib/season/common'
 import SeasonLayout from '@components/season/SeasonLayout'
 import React from 'react'
-
-export async function getStaticProps({ params }): Promise<GetStaticPropsResult<any>> {
-    return {
-        props: {},
-    }
-}
 
 const SeasonVods = (): JSX.Element => {
     return <div>Coming soon</div>
@@ -18,11 +10,3 @@ SeasonVods.layout = (content: React.ReactNode): JSX.Element => {
 }
 
 export default SeasonVods
-
-export async function getStaticPaths(): Promise<GetStaticPathsResult> {
-    const paths = await getSeasonPaths('vods')
-    return {
-        paths,
-        fallback: true,
-    }
-}
